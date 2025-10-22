71c2035c798d3d7a427c0d44adaad2494aed4859
Mikhail Titov <mlt@gmx.us>
2025-10-22 14:19:12 -0500

    Copy do_line from a4

--- allegro4/allegro.c
+++ allegro4/allegro.c
@@ -1090,6 +1090,97 @@ void putpixel(BITMAP * buffer, int x, int y, int color){
     }
 }
 
+/* do_line:
+ *  Calculates all the points along a line between x1, y1 and x2, y2,
+ *  calling the supplied function for each one. This will be passed a
+ *  copy of the bmp parameter, the x and y position, and a copy of the
+ *  d parameter (so do_line() can be used with putpixel()).
+ */
+void do_line(BITMAP *bmp, int x1, int y1, int x2, int y2, int d, void (*proc)(BITMAP *, int, int, int))
+{
+   int dx = x2 - x1;
+   int dy = y2 - y1;
+   int i1, i2;
+   int x, y;
+   int dd;
+
+   /* worker macro */
+   #define DO_LINE(pri_sign, pri_c, pri_cond, sec_sign, sec_c, sec_cond)     \
+   {                                                                         \
+      if (d##pri_c == 0) {                                                   \
+         proc(bmp, x1, y1, d);                                               \
+         return;                                                             \
+      }                                                                      \
+                                                                             \
+      i1 = 2 * d##sec_c;                                                     \
+      dd = i1 - (sec_sign (pri_sign d##pri_c));                              \
+      i2 = dd - (sec_sign (pri_sign d##pri_c));                              \
+                                                                             \
+      x = x1;                                                                \
+      y = y1;                                                                \
+                                                                             \
+      while (pri_c pri_cond pri_c##2) {                                      \
+         proc(bmp, x, y, d);                                                 \
+                                                                             \
+         if (dd sec_cond 0) {                                                \
+            sec_c = sec_c sec_sign 1;                                        \
+            dd += i2;                                                        \
+         }                                                                   \
+         else                                                                \
+            dd += i1;                                                        \
+                                                                             \
+         pri_c = pri_c pri_sign 1;                                           \
+      }                                                                      \
+   }
+
+   if (dx >= 0) {
+      if (dy >= 0) {
+         if (dx >= dy) {
+            /* (x1 <= x2) && (y1 <= y2) && (dx >= dy) */
+            DO_LINE(+, x, <= , +, y, >= );
+         }
+         else {
+            /* (x1 <= x2) && (y1 <= y2) && (dx < dy) */
+            DO_LINE(+, y, <= , +, x, >= );
+         }
+      }
+      else {
+         if (dx >= -dy) {
+            /* (x1 <= x2) && (y1 > y2) && (dx >= dy) */
+            DO_LINE(+, x, <= , -, y, <= );
+         }
+         else {
+            /* (x1 <= x2) && (y1 > y2) && (dx < dy) */
+            DO_LINE(-, y, >= , +, x, >= );
+         }
+      }
+   }
+   else {
+      if (dy >= 0) {
+         if (-dx >= dy) {
+            /* (x1 > x2) && (y1 <= y2) && (dx >= dy) */
+            DO_LINE(-, x, >= , +, y, >= );
+         }
+         else {
+            /* (x1 > x2) && (y1 <= y2) && (dx < dy) */
+            DO_LINE(+, y, <= , -, x, <= );
+         }
+      }
+      else {
+         if (-dx >= -dy) {
+            /* (x1 > x2) && (y1 > y2) && (dx >= dy) */
+            DO_LINE(-, x, >= , -, y, <= );
+         }
+         else {
+            /* (x1 > x2) && (y1 > y2) && (dx < dy) */
+            DO_LINE(-, y, >= , -, x, <= );
+         }
+      }
+   }
+
+#undef DO_LINE
+}
+
 void line(BITMAP * buffer, int x, int y, int x2, int y2, int color){
     draw_into(buffer);
     al_draw_line(x + 0.5, y + 0.5, x2 + 0.5, y2 + 0.5, a5color(color, current_depth), 1);
